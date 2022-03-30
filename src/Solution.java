7272d021e14b997b35cb0a43461ff71561fb705b
you88311 <you88311@naver.com>
2022-03-30 17:38:35 +0900

    초기 작업 완료
    ArrayList 선언하여 주어진 input 넣기 구현

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,19 @@
+import java.util.ArrayList;
+
 public class Solution {
     public String solution(String new_id) {
         String answer = "";
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add(new_id);
+
         return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String new_id =  "...!@BaT#*..y.abcdefghijklm";
+
+        System.out.println(sol.solution(new_id));
     }
 }
