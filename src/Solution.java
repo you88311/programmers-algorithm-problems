24f529d042ca0823f290c59a478b648f27387fa6
you88311 <you88311@naver.com>
2022-03-31 21:54:47 +0900

    no message

--- src/Solution.java
+++ src/Solution.java
@@ -3,7 +3,21 @@ import java.util.ArrayList;
 public class Solution {
     public String solution(String new_id) {
         String answer = "";
-        
+        /*1단계 대문자 -> 소문자*/
+        new_id = new_id.toLowerCase();
+        /*2단계*/
+        System.out.println(new_id);
+
+        return answer;
+    }
+    public String secondStep(String new_id){
+        String answer = "";
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add(new_id);
+
+        for(int i = 0; i < arrayList.size() ; i++){
+            if(arrayList.get(i) != "")
+        }
 
         return answer;
     }
