1508304338d0f0d5dfeb89810cf288ae1b5d98f1
you88311 <you88311@naver.com>
2022-04-06 17:34:44 +0900

    프로그래머스 Lv1 소수 만들기

--- src/Solution.java
+++ src/Solution.java
@@ -3,40 +3,35 @@ import java.util.ArrayList;
 public class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        boolean isPrimeNumber = false;
+        boolean isPrimeNumber;
         ArrayList<Integer> arrayList = new ArrayList<>();
 
         /*조합 만들기*/
         combination(arrayList, nums);
-        System.out.println(arrayList);
         /*조합 중 소수 찾을 시 answer 증가*/
-        for(int i = 0; i < arrayList.size() ; i++){
-            isPrimeNumber = false;
-            for(int j = 2 ; j <= Math.sqrt(arrayList.get(i)) ; j++){
-                if((arrayList.get(i) % j == 0)) {
+        for (int i = 0; i < arrayList.size(); i++) {
+            isPrimeNumber = true;
+            for (int j = 2; j <= Math.sqrt(arrayList.get(i)); j++) {
+                if ((arrayList.get(i) % j == 0)) {
                     isPrimeNumber = false;
                     break;
                 }
-                else
-                    isPrimeNumber = true;
             }
-            answer = (isPrimeNumber == true) ? ++answer : answer;
+            answer += (isPrimeNumber) ? 1 : 0;
         }
-
         return answer;
     }
 
-    void combination (ArrayList<Integer> arrayList, int[] nums){
-        int[] window = {0, 1};
+    void combination(ArrayList<Integer> arrayList, int[] nums) {
         Integer sum;
-        while(window[1] < nums.length){
-            for(int i = window[1]+1 ; i < nums.length ; i++ ){
-                sum = nums[window[0]] + nums[window[1]] + nums[i] ;
-                if(!arrayList.contains(sum))
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    sum = nums[i] + nums[j] + nums[k];
                     arrayList.add(sum);
+                }
             }
-            window[0]++;
-            window[1]++;
         }
     }
 
